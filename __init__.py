a7aed50a91913e47ddcd9403da216599b0d52ea7
Inamura JIN <60126349+InamuraJIN@users.noreply.github.com>
2020-07-29 16:47:44 +0900

    storage&init

--- __init__.py
+++ __init__.py
@@ -15,7 +15,6 @@ bl_info = {
 "version": (3, 1, 2),# プラグインのバージョン
 "blender": (2, 83, 0),# プラグインが動作するBlenderのバージョン
 "location" : "View 3D",# Blender内部でのプラグインの位置づけ
-"description" : "Thank you for using our services",# プラグインの説明
 "warning" : "",
 "wiki_url" : "https://github.com/InamuraJIN/CommandRecorder/blob/master/README.md",# Documentation
 "tracker_url" : "https://twitter.com/Inamura_JIN",# Report Bug
