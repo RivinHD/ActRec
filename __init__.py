b55f9791e19a0d056729db2dc9005f097fd534c4
RivinHD <58261670+RivinHD@users.noreply.github.com>
2020-08-06 12:05:36 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,7 +4,7 @@ from . import ActionRecorder as ActionRecorder
 bl_info = {
     "name" : "ActionRecorder",
     "author" : "BuuGraphic, Rivin",
-    "version": (0, 99, 7),
+    "version": (0, 99, 8),
     "blender": (2, 83, 0),
     "location" : "View 3D",
     "warning" : "",
