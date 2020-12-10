b1ab050a41f66694d7f1797bd36ea8b9215f4161
RivinHD <58261670+RivinHD@users.noreply.github.com>
2020-12-10 23:13:15 +0100

    3.5.16

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 config = {
-    "Manual_URL" : 'https://github.com/InamuraJIN/CommandRecorder/blob/master/README.md',
+    "Manual_URL" : 'https://github.com/InamuraJIN/ActionRecorder/blob/master/README.md',
     "Hint_URL": '',
     "BugReport_URL": '',
     "checkSource_URL": 'https://api.github.com/repos/RivinHD/ActRec/contents/__init__.py',
