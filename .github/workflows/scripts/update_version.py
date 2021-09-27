26467cdee8a31ff884897eba55bb8765427c7ac1
RivinHD <58261670+RivinHD@users.noreply.github.com>
2021-09-27 16:14:56 +0200

    added Action

---
+++ .github/workflows/scripts/update_version.py
@@ -0,0 +1,49 @@
+import argparse
+import os
+import json
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-files", type= list)
+    args = parser.parse_args()
+
+    addon_directory = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+    version = (0, 0, 0)
+    with open(os.path.join(addon_directory, "__init__.py"), 'r', encoding= 'utf-8') as file:
+        for line in file.readlines():
+            if "version" in line:
+                version = eval("%s)" %line.split(":")[1].split(")")[0].strip())
+                break
+    with open(os.path.join(addon_directory, "actrec/config.py")) as file:
+        for line in file.readlines():
+            if "version" in line:
+                check_version = line.split("=")[1].strip()
+                if check_version > version:
+                    version = check_version
+                break
+
+    print("Update to Version %s\nFiles:%s" %(version, args.files))
+
+    version = list(version)
+    with open(os.path.join(addon_directory, "download_file.json"), 'w+', encoding= 'utf-8') as file:
+        data = json.loads(file.read())
+        for file in args.files:
+            data[file] = version
+        json.dump(data, file, ensure_ascii= False, indent= 4)
+    
+    with open(os.path.join(addon_directory, "__init__.py"), 'r+', encoding= 'utf-8') as file:
+        lines = []
+        for line in file.readlines():
+            if "version" in line:
+                split = line.split(": ")
+                sub_split = split[1].split(")")
+                line = "%s: %s%s" %(split[0], tuple(version), sub_split[-1])
+            lines.append(line)
+                
+        
+
+    
+
+    
+    
+
--- __init__.py
+++ __init__.py
@@ -3,13 +3,13 @@
 bl_info = {
     "name" : "ActionRecorder",
     "author" : "InamuraJIN, Rivin",
-    "version": (4, 0, 2), # also update version in config
-    "blender": (2, 83, 12),
+    "version" : (4, 0, 2), # also update version in config
+    "blender" : (2, 83, 12),
     "location" : "View 3D",
     "warning" : "",
     "wiki_url" : 'https://github.com/InamuraJIN/ActionRecorder/blob/master/README.md', # Documentation
     "tracker_url" : 'https://inamurajin.wixsite.com/website/post/bug-report', # Report Bug
-    'link': 'https://twitter.com/Inamura_JIN',
+    'link' : 'https://twitter.com/Inamura_JIN',
     "category" : "System"
 }
 
