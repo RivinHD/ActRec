e880534b0150c81a2f90a8b7681be9ee9319a9f3
RivinHD <58261670+RivinHD@users.noreply.github.com>
2021-09-27 18:19:47 +0200

    update action

--- .github/workflows/scripts/update_version.py
+++ .github/workflows/scripts/update_version.py
@@ -1,10 +1,23 @@
 import argparse
 import os
 import json
+import re
+
+def collapse_json(text, list_length=4):
+    for length in range(list_length):
+        re_pattern = r'\[' + (r'\s*(.+)\s*,' * length)[:-1] + r'\]'
+        re_repl = r'[' + ''.join(r'\{}, '.format(i+1) for i in range(length))[:-2] + r']'
+
+        text = re.sub(re_pattern, re_repl, text)
+
+    return text
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-files", type= list)
+    def parse_list(text):
+        return text.split(",")
+    parser.add_argument("-files", type= parse_list, default= [], nargs='?', const= [])
+    parser.add_argument("-removed", type= parse_list, default= [], nargs='?', const= [])
     args = parser.parse_args()
 
     addon_directory = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
@@ -22,15 +35,23 @@ if __name__ == "__main__":
                     version = check_version
                 break
 
-    print("Update to Version %s\nFiles:%s" %(version, args.files))
+    print("Update to Version %s\nFiles: %s\nRemoved: %s" %(version, args.files, args.removed))
 
     version = list(version)
-    with open(os.path.join(addon_directory, "download_file.json"), 'w+', encoding= 'utf-8') as file:
-        data = json.loads(file.read())
+    with open(os.path.join(addon_directory, "download_file.json"), 'r', encoding= 'utf-8') as download_file:
+        data = json.loads(download_file.read())
+        data_files = data["files"]
         for file in args.files:
-            if data.get(file, None):
-                data[file] = version
-        json.dump(data, file, ensure_ascii= False, indent= 4)
+            if data_files.get(file, None):
+                data_files[file] = version
+        data_remove = data["remove"]
+        for file in args.removed:
+            print(file)
+            if file not in data_remove:
+                data_remove.append(file)
+    with open(os.path.join(addon_directory, "download_file.json"), 'w', encoding= 'utf-8') as download_file:
+        download_file.write(collapse_json(json.dumps(data, ensure_ascii= False, indent= 4)))
+        
     
     with open(os.path.join(addon_directory, "__init__.py"), 'r+', encoding= 'utf-8') as file:
         lines = []
@@ -40,11 +61,3 @@ if __name__ == "__main__":
                 sub_split = split[1].split(")")
                 line = "%s: %s%s" %(split[0], tuple(version), sub_split[-1])
             lines.append(line)
-                
-        
-
-    
-
-    
-    
-
