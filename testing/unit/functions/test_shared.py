02ea83b0ac7dd159b9852a101328453dad2133e3
RivinHD <58261670+RivinHD@users.noreply.github.com>
2022-09-02 23:13:46 +0200

    retry

--- testing/unit/functions/test_shared.py
+++ testing/unit/functions/test_shared.py
@@ -20,7 +20,7 @@ def test_check_for_duplicates(check_list, name, output):
     assert shared.check_for_duplicates(check_list, name) == output
 
 
-"""
+""" # access violation
 @pytest.mark.parametrize("property_str, exclude, output",  # TODO more test Data
                          [("bpy.data.workspaces['Layout'].screens['Layout'].areas[0].spaces[0]", [],
                            {'type': 'PROPERTIES', 'show_locked_time': False, 'show_region_header': True,
@@ -36,8 +36,8 @@ def test_property_to_python(property_str, exclude, output):
 """
 
 
-@pytest.mark.parametrize("property_str, data",
-                         [("bpy.data.workspaces['Layout'].screens['Layout'].areas[0].spaces[0]",
+@pytest.mark.parametrize("property, data",
+                         [(bpy.data.workspaces['Layout'].screens['Layout'].areas[0].spaces[0],
                            {'type': 'PROPERTIES', 'show_locked_time': True, 'show_region_header': False,
                             'context': 'DATA', 'pin_id': None, 'use_pin_id': False,
                             'tab_search_results':
@@ -45,18 +45,16 @@ def test_property_to_python(property_str, exclude, output):
                              False, False, False, False, False, False),
                             'search_filter': '', 'outliner_sync': 'AUTO'})]
                          )
-def test_apply_data_to_item(property_str, data):
-    property = eval(property_str)
+def test_apply_data_to_item(property, data):
     shared.apply_data_to_item(property, data)
     assert helper.compare_with_dict(property, data)
 
 
-@pytest.mark.parametrize("collection_str, data",
-                         [("bpy.context.preferences.addons['cycles'].preferences.devices",
+@pytest.mark.parametrize("collection, data",
+                         [(bpy.context.preferences.addons['cycles'].preferences.devices,
                            {'name': "test", 'id': "TT", 'use': False, 'type': "OPTIX"})]
                          )
-def test_add_data_to_collection(collection_str, data):
-    collection = eval(collection_str)
+def test_add_data_to_collection(collection, data):
     length = len(collection)
     name = data['name']
     index = collection.find(name)
