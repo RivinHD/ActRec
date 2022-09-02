7485abbfaeb305a1d8d349a7a84ff733f197f813
RivinHD <58261670+RivinHD@users.noreply.github.com>
2022-09-02 17:14:06 +0200

    retry

--- testing/unit/test_icon_manager.py
+++ testing/unit/test_icon_manager.py
@@ -38,8 +38,7 @@ def test_get_icons_names():
 )
 def test_load_icon(file, name, only_new, success):
     # include register_icon testing
-    dirpath = "test_src_data/icon_manager"
-    path = os.path.join(dirpath, file)
+    path = "test_src_data/icon_manager/%s" % file
     pref = get_preferences(bpy.context)
     pref.icon_path = os.path.dirname(__file__)
     icon_manager.load_icon(pref, path, only_new)
