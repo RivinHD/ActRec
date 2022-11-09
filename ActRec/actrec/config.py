d3ed34805251be38eb5279e1abbedada4de0eea3
RivinHD <58261670+RivinHD@users.noreply.github.com>
2022-11-09 20:02:19 +0100

    Update config.py

--- ActRec/actrec/config.py
+++ ActRec/actrec/config.py
@@ -2,6 +2,6 @@ manual_url = 'https://github.com/InamuraJIN/ActionRecorder/blob/master/README.md
 hint_url = 'https://inamurajin.wixsite.com/website/post/hint-help'
 bug_report_url = 'https://inamurajin.wixsite.com/website/post/bug-report'
 check_source_url = 'https://raw.githubusercontent.com/RivinHD/ActRec/master/download_file.json'
-repo_source_url = 'https://raw.githubusercontent.com/RivinHD/ActRec/master/%s'
+repo_source_url = 'https://raw.githubusercontent.com/RivinHD/ActRec/master/ActRec/%s'
 release_notes_url = 'https://github.com/InamuraJIN/ActionRecorder/wiki'
 version = (4, 0, 0)
